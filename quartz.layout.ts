4c5170c4b438fa230c74bf80772f15d78ce1b47f
troyejames9 <130838331+TroyeJames9@users.noreply.github.com>
2025-07-26 08:14:36 +0800

    Quartz sync: Jul 26, 2025, 8:14 AM

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -8,7 +8,7 @@ export const sharedPageComponents: SharedLayout = {
   afterBody: [],
   footer: Component.Footer({
     links: {
-      粤公网安备xxxxxxx号: "https://beian.mps.gov.cn/#/query/webSearch?code=xxxxxx",
+      粤公网安备44011102484362号: "https://beian.mps.gov.cn/#/query/webSearch?code=44011102484362",
       "粤ICP备2025445261号": "https://beian.miit.gov.cn/",
     },
   }),
