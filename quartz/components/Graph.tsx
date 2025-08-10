103553c5872e7e227f6e50b0421f9dddbe4dbdde
troyejames9 <130838331+TroyeJames9@users.noreply.github.com>
2025-08-10 16:19:03 +0800

    Quartz sync: Aug 10, 2025, 4:19 PM

--- quartz/components/Graph.tsx
+++ quartz/components/Graph.tsx
@@ -75,7 +75,7 @@ export default ((opts?: Partial<GraphOptions>) => {
               xmlnsXlink="http://www.w3.org/1999/xlink"
               x="0px"
               y="0px"
-              viewBox="0 0 55 55"
+              viewBox="0 0 65 65"
               fill="currentColor"
               xmlSpace="preserve"
             >
