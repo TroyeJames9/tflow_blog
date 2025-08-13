05502b53a2b1d5953ea4357b9bc0547ff02c9e93
troyejames9 <130838331+TroyeJames9@users.noreply.github.com>
2025-08-13 08:15:42 +0800

    Quartz sync: Aug 13, 2025, 8:15 AM

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -39,7 +39,15 @@ export const defaultContentPageLayout: PageLayout = {
         { Component: Component.ReaderMode() },
       ],
     }),
-    Component.Explorer(),
+    Component.Explorer({
+      mapFn: (node) => {
+        if (node.isFolder) {
+          node.displayName = "📁 " + node.displayName
+        } else {
+          node.displayName = "📄 " + node.displayName
+        }
+      },
+    }),
   ],
   right: [
     Component.Graph(),
