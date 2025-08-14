88227ba7e2dd0aa0f42e12c0d29bb2761a3b146f
troyejames9 <130838331+TroyeJames9@users.noreply.github.com>
2025-08-14 18:19:34 +0800

    Quartz sync: Aug 14, 2025, 6:19 PM

--- quartz.config.ts
+++ quartz.config.ts
@@ -66,7 +66,6 @@ const config: QuartzConfig = {
         },
         keepBackground: false,
       }),
-      Plugin.CodeCopy(),
       Plugin.ObsidianFlavoredMarkdown({ enableInHtmlEmbed: false }),
       Plugin.GitHubFlavoredMarkdown(),
       Plugin.TableOfContents(),
