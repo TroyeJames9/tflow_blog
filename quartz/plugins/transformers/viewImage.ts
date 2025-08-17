c24c818ef864180b4b905a5118f4d1914be46833
troyejames9 <130838331+TroyeJames9@users.noreply.github.com>
2025-08-17 08:49:49 +0800

    Quartz sync: Aug 17, 2025, 8:49 AM

--- quartz/plugins/transformers/viewImage.ts
+++ quartz/plugins/transformers/viewImage.ts
@@ -9,7 +9,7 @@ export const ViewImage: QuartzTransformerPlugin = () => {
       return {
         js: [
           {
-            src: "https://cdn.jsdelivr.net/gh/Tokinx/ViewImage/view-image.min.js",
+            src: "https://cdn.jsdelivr.net/gh/Tokinx/ViewImage/view-image.js",
             loadTime: "afterDOMReady",
             contentType: "external",
           },
