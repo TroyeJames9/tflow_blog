1203ed3bd038dd8dcf8fdffe7e3af6a174ca337a
troyejames9 <130838331+TroyeJames9@users.noreply.github.com>
2025-08-17 09:07:59 +0800

    Quartz sync: Aug 17, 2025, 9:07 AM

--- quartz/plugins/transformers/viewImage.ts
+++ quartz/plugins/transformers/viewImage.ts
@@ -10,7 +10,7 @@ export const ViewImage: QuartzTransformerPlugin = () => {
         js: [
           {
             src: "https://cdn.jsdelivr.net/gh/Tokinx/ViewImage/view-image.js",
-            loadTime: "afterDOMReady",
+            loadTime: "beforeDOMReady",
             contentType: "external",
           },
           {
@@ -30,7 +30,7 @@ export const ViewImage: QuartzTransformerPlugin = () => {
                 }
               });
             `,
-            loadTime: "afterDOMReady",
+            loadTime: "beforeDOMReady",
             contentType: "inline",
           },
         ],
