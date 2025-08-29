fc31b939a2f564a0b30dfca668b79a6610ec5960
troyejames9 <130838331+TroyeJames9@users.noreply.github.com>
2025-08-29 17:14:48 +0800

    Quartz sync: Aug 29, 2025, 5:14 PM

--- quartz/plugins/transformers/links.ts
+++ quartz/plugins/transformers/links.ts
@@ -93,10 +93,11 @@ export const CrawlLinks: QuartzTransformerPlugin<Partial<Options>> = (userOpts) 
                   classes.push("alias")
                 }
                 node.properties.className = classes
-
-                if (isExternal && opts.openLinksInNewTab) {
-                  node.properties.target = "_blank"
-                }
+                
+                // 内外链接都从新窗口跳转
+                // if (isExternal && opts.openLinksInNewTab) {
+                node.properties.target = "_blank"
+                // }
 
                 // don't process external links or intra-document anchors
                 const isInternal = !(isAbsoluteUrl(dest) || dest.startsWith("#"))
